897081a2ab3b3548b40c45acf9c32bd0578bf3d5
Vika Kapitulina <vika.kapitulina@gmail.com>
2018-06-13 12:40:29 +0300

    Add scroll to bottom slider

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,10 +1,45 @@
 $(document).ready(function () {
 	// Top slider with movie 
 
-	$('.js-index-slider').slick({
+	$('.slider-top .js-index-slider').slick({
 	  infinite: true,
-	  nextArrow: '.index-slider__controls-next',
-	  prevArrow: '.index-slider__controls-prev',
+	  nextArrow: '.slider-top .index-slider__controls-next',
+	  prevArrow: '.slider-top .index-slider__controls-prev',
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  autoplay: true,
+  	  autoplaySpeed: 2000,
+  	  responsive: [
+  	  	{
+	      breakpoint: 1024,
+	      settings: {
+	        arrows: false,
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        slidesToShow: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+
+	// Bottom slider with movie 
+
+	$('.slider-bottom .js-index-slider').slick({
+	  infinite: true,
+	  nextArrow: '.slider-bottom .index-slider__controls-next',
+	  prevArrow: '.slider-bottom .index-slider__controls-prev',
 	  slidesToShow: 3,
 	  slidesToScroll: 1,
 	  autoplay: true,
